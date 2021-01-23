c132827b9a7cbba9593b438cf6e632f92c6e2ec7
li771547 <771547353@qq.com>
2021-01-23 18:26:41 +0800

    init swiper

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <home-header></home-header>
+    <swiper-map></swiper-map>
   </div>
 </template>
 
 <script>
 
 import HomeHeader from '@/components/HomeHeader'
+import SwiperMap from '../components/Swiper'
 
 export default {
   name: 'Home',
   components: {
+    SwiperMap,
     HomeHeader
   }
 }
