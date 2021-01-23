19df6fbcfc6c4acfc2b10dacd36db623ca6629b6
li771547 <771547353@qq.com>
2021-01-23 18:03:28 +0800

    init header

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont icon-back">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      <input type="text" placeholder="输入城市/景点/游玩主题" class="search">
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display :flex
+    height: 43px
+    line-height: 43px
+    background-color :#25a4bb
+    text-align center
+    .header-left
+      float : left
+      width: 43px
+      .icon-back
+        font-size: 20px
+    .header-input
+      display :flex;
+      flex :1
+      height : 32px
+      line-height: 32px
+      margin-top : 6px
+      margin-left :5px
+      background-color :#ffffff
+      .search
+        width: 100%
+        height: 100%
+        box-sizing :border-box
+        padding :4px
+    .header-right
+      float : right
+      width: 60px
+</style>
