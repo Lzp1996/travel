4e8162e0daab4d8f4316e253ac72ba6d3fa91090
li771547 <771547353@qq.com>
2021-01-24 23:25:54 +0800

    finish detail-header

---
+++ src/components/DetailHeader.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <router-link tag="div" v-show="showAbs" to="/" class="header-abs">
+      <div class="iconfont icon-back">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      景点详情
+      <router-link tag="div" to="/">
+        <div class="iconfont icon-fixed-back">&#xe624;</div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.showAbs = false
+        this.opacityStyle = opacity
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header-abs
+    position: absolute;
+    top :.1rem
+    left :.2rem
+    width: .8rem
+    height: .8rem
+    line-height: .8rem;
+    text-align :center
+    background-color : rgba(0,0,0, .8)
+    border-radius :.4rem
+    .icon-back
+      color :#ffffff
+  .header-fixed
+    position :fixed
+    top :0
+    width: 100%
+    height: .86rem
+    line-height: .86rem
+    text-align: center
+    color :#ffffff
+    background-color :#25a4bb
+    .icon-fixed-back
+      position : absolute
+      top :0
+      left :0
+      width: .64rem
+      text-align :center
+      font-size: .5rem;
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
+  <div >
     <banner></banner>
+    <detail-header></detail-header>
+    <div class="h"></div>
   </div>
 </template>
 
 <script>
 import Banner from '@/components/Banner'
+import DetailHeader from '@/components/DetailHeader'
 export default {
   name: 'Detail',
-  components: { Banner }
+  components: { DetailHeader, Banner }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+  .h
+    height : 5000px
 </style>
